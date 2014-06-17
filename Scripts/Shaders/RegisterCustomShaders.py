9d10643a1a857069fc6bb6719c5f2a473af90645
DigiTecK3D <miguel@digiteck3d.com>
2014-06-17 00:35:44 -0500

    BSD License Header Switch and Reformatting
    Switching the entire library over to the BSD style license header since
    the GPL is slightly stricter. Bringing over the reformatted versions
    that have been integrated into the  IdeascaleNodePack.

--- Scripts/Shaders/RegisterCustomShaders.py
+++ Scripts/Shaders/RegisterCustomShaders.py
@@ -1,6 +1,9 @@
 # ------------------------------------------------------------------------------
 # Mari Shader Library Registration Master
-# Copyright (c) 2013 DigiTecK3D. All Rights Reserved.
+# Copyright (c) 2013 Mari Ideascale. All Rights Reserved.
+# ------------------------------------------------------------------------------
+# File: RegisterCustomShaders.py
+# Description: Our shader register script to auto-load our library into Mari.
 # ------------------------------------------------------------------------------
 # Author: Ben Neall
 # Web: www.bneall.blogspot.com
@@ -14,33 +17,46 @@
 # Master Git: https://github.com/DigiTecK3D/DT3D_Mari-Procedural-Library
 # ------------------------------------------------------------------------------
 # History:
-# - 02/04/14    New Auto-Loader Script Intergration.
+# - 02/04/14    New Auto-Loader Script Integration.
+# - 06/14/14    Update Header To New BSD License.
 # ------------------------------------------------------------------------------
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF HE POSSIBILITY OF SUCH DAMAGE.
 # ------------------------------------------------------------------------------
 
 import mari
 import os
-import glob
 import xml.etree.ElementTree as ET
 
 # global variables
 base_path = os.path.dirname(__file__)
 default_shader_path = '%s/NodeLibrary' % base_path
 default_lib_path = '%s/FunctionLibrary' % base_path
-lib_version = '1.06'
 min_mari_version = '2.5'
+current_lib_version = '1.06'
 current_mari_version = '2.5'
 
 def mariVersion():
@@ -56,35 +72,37 @@ def libNamerNode():
 
     # check to see if library is in path
     libNamer = os.path.isfile(default_lib_path + "/LibraryNamer.xml")
-    if libNamer: # found library parse xml file
+    if libNamer:  # found library parse xml file
         xml = ET.parse('%s/%s' % (default_lib_path, "LibraryNamer.xml"))
         root = xml.getroot()
+    else:
+        root = ""
 
-    return (libNamer,root) # return check, xml root
-        
+    return (libNamer, root)  # return check, xml root
+
 def libVersion():
     '''Loads current library version'''
 
     # grab our global variable
-    global default_lib_version
+    global current_lib_version
 
     # run check and grab xml
     libNamer, root = libNamerNode()
 
     # if check grab version
-    if libNamer: 
+    if libNamer:
         # check for lib version in xml
-        libVersion = root.find("LibraryVersion")
-        if libVersion != None: 
+        libVersion = (root or "").find("LibraryVersion")
+        if libVersion != None:
             # lib xml version found
-            default_lib_version = libVersion.text
+            current_lib_version = libVersion.text
             # print '-----------------------------------------'
-            # print 'Library Version Found v' + (default_lib_version)
-        else: 
+            # print 'Library Version Found v' + (current_lib_version)
+        else:
             # lib version min default
             print '-----------------------------------------'
             print 'Library Version Not Set'
-            print 'Using Min Version v' + (default_lib_version)
+            print 'Using Min Version v' + (current_lib_version)
 
 def loadLibraries():
     '''Loads custom shader libraries'''
@@ -96,7 +114,7 @@ def loadLibraries():
     libDict = {}
     for path, subdirs, files in os.walk(default_lib_path):
         for name in files:
-            libDict[name]=path
+            libDict[name] = path
 
     # find the library paths for the libraries
     for lib in libDict:
@@ -123,10 +141,10 @@ def loadLibraries():
         # files so we can load them up
         try:
             if lib.endswith('glslh'):
-            	# function library glsl header file
+                # function library glsl header file
                 mari.gl_render.registerCustomHeaderFile(libNode, libPath)
             elif lib.endswith('glslc'):
-            	# function library glsl source file
+                # function library glsl source file
                 mari.gl_render.registerCustomCodeFile(libNode, libPath)
                 print 'Registered Library: %s' % libName
         except Exception as exc:
@@ -139,19 +157,22 @@ def loadShaders():
     shaderDict = {}
     for path, subdirs, files in os.walk(default_shader_path):
         for name in files:
-            shaderDict[name]=path
+            shaderDict[name] = path
 
     # storage for our found nodes to call in order later
     nodePs, nodeAd, nodeDf, nodeSp, nodeSt = [], [], [], [], []
 
     # determine attributes
-    for shader in shaderDict: 
+    for shader in shaderDict:
         full_shader_path = shaderDict[shader]
-        xml = ET.parse('%s/%s' % (full_shader_path, shader))
+        if shader.endswith('xml'):
+            xml = ET.parse('%s/%s' % (full_shader_path, shader))
+        else:
+            continue
         root = xml.getroot()
 
         # check for the name id tag
-        shaderName = root.find('ID')
+        shaderName = root.find('DefaultName')
         # check to see if tag is set
         if shaderName != None:
             # grab our xml set name
@@ -183,16 +204,16 @@ def loadShaders():
         # if procedural or adjustment grab shader category info
         if shaderType == 'Procedural' or shaderType == 'Adjustment':
             # check for the category tag
-	        shaderPath = root.find('Category')
+            shaderPath = root.find('Category')
             # check to see if tag is set
-	        if shaderPath != None:
+            if shaderPath != None:
                 # grab our xml set category
-	            shaderPath = shaderPath.text + shaderName
-	        else:
+                shaderPath = shaderPath.text + shaderName
+            else:
                 # no category set in node
-	            print 'XML Category Not Set'
+                print 'XML Category Not Set'
 
-	    # grab our node path to the shader
+        # grab our node path to the shader
         nodePath = "%s/%s" % (full_shader_path, shader)
         nodePath = nodePath.replace("\\", "/")
 
@@ -200,20 +221,20 @@ def loadShaders():
         # into our array so we can call them later
         try:
             if shaderType == 'Procedural':
-            	# procedural node
-                nodePs.append((shaderType,shaderName,shaderPath,nodePath))
+                # procedural node
+                nodePs.append((shaderType, shaderName, shaderPath, nodePath))
             elif shaderType == 'Adjustment':
-            	# adjustment node
-                nodeAd.append((shaderType,shaderName,shaderPath,nodePath))
+                # adjustment node
+                nodeAd.append((shaderType, shaderName, shaderPath, nodePath))
             elif shaderType == 'Diffuse':
-            	# diffuse shader node
-                nodeDf.append((shaderType,shaderName,nodePath))
+                # diffuse shader node
+                nodeDf.append((shaderType, shaderName, nodePath))
             elif shaderType == 'Specular':
-            	# specular shader node
-                nodeSp.append((shaderType,shaderName,nodePath))
+                # specular shader node
+                nodeSp.append((shaderType, shaderName, nodePath))
             elif shaderType == 'Standalone':
-            	# standalone shader node
-                nodeSt.append((shaderType,shaderName,nodePath))
+                # standalone shader node
+                nodeSt.append((shaderType, shaderName, nodePath))
         except Exception as exc:
             print 'Error Storing %s Node : %s : %s' % (shaderType, shaderName, str(exc))
 
@@ -226,7 +247,7 @@ def loadShaders():
         print '-----------------------------------------'
         for adjustments in nodeAd:
             try:
-                mari.gl_render.registerCustomAdjustmentLayerFromXMLFile(adjustments[2],adjustments[3])
+                mari.gl_render.registerCustomAdjustmentLayerFromXMLFile(adjustments[2], adjustments[3])
                 print 'Registered %s Node: %s' % (adjustments[0], adjustments[1])
             except Exception as exc:
                 print 'Error Registering %s Node : %s : %s' % (adjustments[0], adjustments[1], str(exc))
@@ -241,7 +262,7 @@ def loadShaders():
         print '-----------------------------------------'
         for procedurals in nodePs:
             try:
-                mari.gl_render.registerCustomProceduralLayerFromXMLFile(procedurals[2],procedurals[3])
+                mari.gl_render.registerCustomProceduralLayerFromXMLFile(procedurals[2], procedurals[3])
                 print 'Registered %s Node: %s' % (procedurals[0], procedurals[1])
             except Exception as exc:
                 print 'Error Registering %s Node : %s : %s' % (procedurals[0], procedurals[1], str(exc))
@@ -255,7 +276,7 @@ def loadShaders():
         print '-----------------------------------------'
         for diffuse in nodeDf:
             try:
-                mari.gl_render.registerCustomDiffuseShaderFromXMLFile(diffuse[1],diffuse[2])
+                mari.gl_render.registerCustomDiffuseShaderFromXMLFile(diffuse[1], diffuse[2])
                 print 'Registered %s Node: %s' % (diffuse[0], diffuse[1])
             except Exception as exc:
                 print 'Error Registering %s Node : %s : %s' % (diffuse[0], diffuse[1], str(exc))
@@ -269,7 +290,7 @@ def loadShaders():
         print '-----------------------------------------'
         for specular in nodeSp:
             try:
-                mari.gl_render.registerCustomSpecularShaderFromXMLFile(specular[1],specular[2])
+                mari.gl_render.registerCustomSpecularShaderFromXMLFile(specular[1], specular[2])
                 print 'Registered %s Node: %s' % (specular[0], specular[1])
             except Exception as exc:
                 print 'Error Registering %s Node : %s : %s' % (specular[0], specular[1], str(exc))
@@ -283,7 +304,7 @@ def loadShaders():
         print '-----------------------------------------'
         for standalone in nodeSt:
             try:
-                mari.gl_render.registerCustomStandaloneShaderFromXMLFile(standalone[1],standalone[2])
+                mari.gl_render.registerCustomStandaloneShaderFromXMLFile(standalone[1], standalone[2])
                 print 'Registered %s Node: %s' % (standalone[0], standalone[1])
             except Exception as exc:
                 print 'Error Registering %s Node : %s : %s' % (standalone[0], standalone[1], str(exc))
@@ -296,17 +317,17 @@ if current_mari_version >= min_mari_version:
     # load all functions and shaders
     # Print our info into the console
     print ""
-    libVersion() # call and check for library version
+    libVersion()  # call and check for library version
     print '-----------------------------------------'
-    print "Mari Shader Library v" + lib_version
+    print "Mari Shader Library v" + current_lib_version
     print '-----------------------------------------'
     print "DigiTecK3D     : www.digiteck3d.com"
     print "Mari Ideascale : www.mari.ideascale.com"
     print '-----------------------------------------'
     print 'Functions Library : Loading'
     print '-----------------------------------------'
-    loadLibraries() # load up our function library
-    loadShaders() # load up our shader library
+    loadLibraries()  # load up our function library
+    loadShaders()  # load up our shader library
     print '-----------------------------------------'
     print ""
 
